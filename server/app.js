b679d294db6f32917e1fbb30f6df71e7ad1de1ce
Joseph Giwoyna <josephgiwoyna@Josephs-MacBook-Pro-2.local>
2016-11-14 15:52:00 -0600

    first commit

--- server/app.js
+++ server/app.js
@@ -1,3 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlencoded( { extended: false } );
+var path = require('path');
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+  console.log('server up on:', port);
+});
+
+app.get('/', function(req, res){
+  res.sendFile(path.resolve('server/public/views/index.html'));
+});
+
+app.use(express.static('server/public'));
+
 // initial jokes provided by the client
 var jokes = [
   {
@@ -16,3 +34,22 @@ var jokes = [
     punchLine: "If you pee on them they disappear."
   }
 ];
+
+app.post('/jokes', function(req, res){
+  console.log(req.body);
+  var newJoke = req.body;
+  res.sendStatus(200);
+});
+
+app.get('/jokes', function(req, res){
+  console.log('get dem jokes');
+  res.send(jokes);
+});
+
+app.get('/*', function(req, res) {
+  var file = req.params[0] || '/server/public/views/index.html';
+  console.log(file);
+
+  res.sendFile(path.join(__dirname, './server/public/', file));
+  // /public/views/index.html
+});
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,57 @@
 console.log('js loaded');
+
+$(document).ready(function (){
+
+  $("#jokes").on("click", function(event) {
+    event.preventDefault();
+    var newJoke = {};
+
+    $.each($('#jokes').serializeArray(), function(i, field) {
+      newJoke[field.name] = field.value;
+    });
+
+    var newJoke = {};
+
+    $.ajax ({
+      type: 'POST',
+      url: '/jokes',
+      data: newJoke,
+      success: function(response){
+        if (response == "created") {
+          getJokes();
+        } else {
+          alert("didn't work");
+        }
+
+      }
+    });
+
+  });
+
+      getJokes();
+
+      function getJokes () {
+        console.log("joke");
+        $.ajax({
+          type: 'GET',
+          url: '/jokes',
+          success: function(jokeData) {
+            console.log(jokeData);
+            jokesToDom(jokeData);
+          }
+        });
+      }
+
+      function jokesToDom(jokes){
+        // $("#jokes").empty();
+        for (var i = 0; i < jokes.length; i++) {
+          $("#jokes").append('<div class="joke"></div>');
+          var $el = $("#jokes").children().last();
+          $el.append('<h3>' + jokes[i].whoseJoke + '</h3>');
+          $el.append('<p>' + jokes[i].jokeQuestion + '</p>');
+          $el.append('<p>' + jokes[i].punchLine + '</p>');
+          console.log(jokes);
+        }
+      };
+
+});
